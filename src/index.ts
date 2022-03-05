5b4f5c775048ac07def632d2f24fe0895b3b47b1
Kevin Farrugia <hello@imkev.dev>
2022-03-05 23:35:29 +0100

    Added CONTRIBUTING.md

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { CSSProperties } from "react";
+
+export { ReactStickyChildren } from "./ReactStickyChildren";
+
+export interface OwnProps {
+  /** Initial style to be applied on the element when it is beneath or within the viewport. (Default: { opacity: 0, visibility: "hidden" }) */
+  initialStyle: CSSProperties;
+  /** Style to be applied on the element when it intersects with the top of the viewport. (default: { opacity: 1, visibility: "visible" }) */
+  intersectingStyle: CSSProperties;
+  /** The IntersectionObserver interface's read-only `root` property identifies the Element or Document whose bounds are treated as the bounding box of the viewport for the element which is the observer's target. If the `root` is null, then the bounds of the actual document viewport are used. */
+  root?: Element | null;
+  /** Top margin from the root in px. (Default: 0) */
+  top?: number;
+  /** Disconnect the IntersectionObserver after one intersection. */
+  once?: boolean;
+
+  className?: string;
+}
+
+export type Props = OwnProps;
